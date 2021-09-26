434367eea0779bdea8031ae882a2436f19f543b3
nhat1206 <nhnhat1206@gmail.com>
2021-09-26 14:36:03 +0700

    register

---
+++ src/main/java/controller/AuthController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import service.TokenService;
+import service.UserService;
+import util.JwtUtil;
+
+@RestController
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user){
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+
+        return userService.createUser(user);
+    }
+}
---
+++ src/main/java/util/JwtUtil.java
@@ -0,0 +1,84 @@
+package util;
+
+import authen.UserPrincipal;
+import org.slf4j.LoggerFactory;
+import org.springframework.expression.ParseException;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+@Component
+public class JwtUtil {
+    private static Logger logger = (Logger) LoggerFactory.getLogger(JwtUtil.class);
+    private static final String USER = "nhat";
+    private static final String SECRET = "hey Mr Nhat the secrect length must be at least 256 bits" +
+            " please no reveal!";
+
+    /**public String generateToken(UserPrincipal user) {
+        String token = null;
+        try {
+            JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder();
+
+            builder.claim(USER, user);
+            builder.expirationTime(generateExpirationDate());
+            JWTClaimsSet claimsSet = builder.build();
+
+            SignedJWT signedJWT = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+            JWSSigner signer = new MACSigner(SECRET.getBytes());
+            signedJWT.sign(signer);
+
+            token = signedJWT.serialize();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return token;
+    }
+
+    public Date generateExpirationDate() {
+        return new Date(System.currentTimeMillis() + 864000000);
+    }
+
+
+    //--------------------getClaimsFromToken-------------------------
+    private JWTClaimsSet getClaimsFromToken(String token) {
+        JWTClaimsSet claims = null;
+        try {
+            SignedJWT signedJWT = SignedJWT.parse(token);
+            JWSVerifier verifier = new MACVerifier(SECRET.getBytes());
+            if (signedJWT.verify(verifier)) {
+                claims = signedJWT.getJWTClaimsSet();
+            }
+        } catch (ParseException | JOSEException e) {
+            logger.error(e.getMessage());
+        }
+        return claims;
+    }
+
+    //--------------------getUserFromToken-------------------------
+    public UserPrincipal getUserFromToken(String token) {
+        UserPrincipal user = null;
+        try {
+            JWTClaimsSet claims = getClaimsFromToken(token);
+            if (claims != null && isTokenExpired(claims)) {
+                JSONObject jsonObject = (JSONObject) claims.getClaim(USER);
+                user = new ObjectMapper()
+                        .readValue(jsonObject.toJSONString(),UserPrincipal.class);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        return user;
+    }
+
+    //--------------------getExpirationDateFromToken-------------------------
+    private Date getExpirationDateFromToken(JWTClaimsSet claims) {
+        return claims != null ? claims.getExpirationTime() : new Date();
+    }
+
+    //--------------------isTokenExpired-------------------------
+    private boolean isTokenExpired(JWTClaimsSet claims) {
+        return getExpirationDateFromToken(claims).after(new Date());
+    }
+     **/
+}
